6ca9036b78734fbdcf4c79f4f44a173ef4a7c823
96olbap <alvinwanjala@gmail.com>
2022-04-11 02:05:15 +0300

    This commit will update the period pipe to calculate the time passed since creation of the quote.

--- src/app/period.pipe.ts
+++ src/app/period.pipe.ts
@@ -3,10 +3,19 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
   name: 'period'
 })
-export class PeriodPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+export class PeriodPipe implements PipeTransform {
+    gettime() {
+      return new Date();
+    }
+  
+    transform(value: any): any {
+      if (value) {
+        let time: any = this.gettime();
+        let diff = Math.abs(time - value);
+        let diffInsec = diff * 0.001;
+        return diffInsec.toFixed(3);
+      }
+    }
   }
 
-}
