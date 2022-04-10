d6a0435f1c730d05546cf13064a3c5389f8c0d2a
96olbap <alvinwanjala@gmail.com>
2022-04-10 03:45:02 +0300

    add gighlight directive

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -8,6 +8,12 @@ import { Quote } from '../quote';
 })
 export class QuotesComponent implements OnInit {
 
+  quotes: Quote[] =[
+    new Quote(1, 'We write to taste life twice, in the moment and in retrospect.', 0, 0),
+    new Quote(2, 'If my doctor told me I had only six minutes to live, I would not brood. I would type a little faster.', 0, 0 ),
+    new Quote(3, 'The most important things in life are mostly thr most hopeless..', 0, 0),
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
