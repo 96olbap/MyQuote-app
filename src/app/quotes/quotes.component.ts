7d9d40e6004c6578556442605252a410f818ad24
96olbap <alvinwanjala@gmail.com>
2022-04-12 15:35:28 +0300

    add buttons to the upvote and downvote wlwmwnt

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Quote } from '../quote';
 
 @Component({
@@ -9,15 +9,23 @@ import { Quote } from '../quote';
 export class QuotesComponent implements OnInit {
 
   quotes: Quote[] =[
-    new Quote(1, 'We write to taste life twice, in the moment and in retrospect.', 5, 1, 'Wepukhulu'),
-    new Quote(2, 'If my doctor told me I had only six minutes to live, I would not brood. I would type a little faster.', 7, 0, 'Carla'),
-    new Quote(3, 'The most important things in life are mostly the most hopeless..', 6, 0, 'Nasim'),  ]
+    new Quote(1, 'We write to taste life twice, in the moment and in retrospect.', 0, 0, 'Wepukhulu'),
+    new Quote(2, 'If my doctor told me I had only six minutes to live, I would not brood. I would type a little faster.', 0, 0, 'Carla'),
+    new Quote(3, 'The most important things in life are mostly the most hopeless..', 0, 0, 'Nasim'),  ]
   addNewQuote(quote:any){
     let quoteLength = this.quotes.length;
     quote.id = quoteLength+1;
     // quote.completeDate = new Date(gquote.completeDate)
     this.quotes.push(quote)
   }
+  @Input() quote!: Quote;
+  // upvote(){
+  //   let upvoteCount = this.quotes.upvote+=1;
+  // }
+
+  // downvote(){
+  //   this.quotes.downvote-=1
+  // }
 
   arr: number[] = this.quotes.map(
     function (quote){
@@ -29,6 +37,7 @@ export class QuotesComponent implements OnInit {
   showAuthor(index: any){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
   }
+
 
   deleteQuote(isComplete: any, index: number){
     if (isComplete) {
